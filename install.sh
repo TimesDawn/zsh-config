16c378ac327a20acb58971733b92bad3b01d1540
TimesDawn <marcantoine.frenette@gmail.com>
2026-07-01 22:24:10 -0400

    completed

--- install.sh
+++ install.sh
@@ -2,24 +2,24 @@
 
 echo "Starting zsh installation & configuration..."
 
-sudo pacman -S zsh fzf ripgrep zinit bat starship curl jq --needed
+sudo pacman -S zsh fzf fd ripgrep eza zinit bat starship curl jq --needed
 
-curl -O --output-dir /etc/zsh https://raw.githubusercontent.com/TimesDawn/zsh-config/refs/heads/main/zshenv
+sudo curl -O --output-dir /etc/zsh https://raw.githubusercontent.com/TimesDawn/zsh-config/refs/heads/main/zshenv
 
 if [[ ! -d "$HOME/.config/zsh" ]]; then
   mkdir -p "$HOME/.config/zsh"
 fi
 
-if [[ ! -d "$HOME/.cache "]]; then
-  mkdir -p "$HOME/.cache"
+if [[ ! -d "$HOME/.cache/zsh"]]; then
+  mkdir -p "$HOME/.cache/zsh"
 fi
 
-if [[ ! -d "$HOME/.local/share "]]; then
-  mkdir -p "$HOME/.local/share"
+if [[ ! -d "$HOME/.local/share/zsh"]]; then
+  mkdir -p "$HOME/.local/share/zsh"
 fi
 
-if [[ ! -d "$HOME/.local/state "]]; then
-  mkdir -p "$HOME/.local/state" 
+if [[ ! -d "$HOME/.local/state/zsh"]]; then
+  mkdir -p "$HOME/.local/state/zsh" 
 fi
 
 for url in $(curl -s https://api.github.com/repos/TimesDawn/zsh-config/contents/zsh | jq -r '.[] | select(.type=="file") | .download_url'); do
